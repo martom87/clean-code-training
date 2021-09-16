476b221876e72b1c9d888c706a6fef3bbd72cb99
Marcin Tomala <marcintomala87@gmail.com>
2021-09-16 17:08:56 +0200

    Solve exercise 4b

--- Exercise 4b. Extract Conditions.rb
+++ Exercise 4b. Extract Conditions.rb
@@ -4,8 +4,24 @@ class PhoneNumberFormatValidator < ActiveModel::Validator
   PHONE_REGEX = /^[0-9]{9}$/
 
   def validate(record)
-    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+    unless valid_phone_number?(record)
       record.errors[:phone_number] << "invalid phone number format"
     end
   end
-end 
+
+  private
+
+  def valid_phone_number?(record)
+    return true if empty_phone_number?(record)
+
+    correctly_formatted_phone_number?(record)
+  end
+
+  def empty_phone_number?(record)
+    record.phone_number.nil?
+  end
+
+  def correctly_formatted_phone_number?(record)
+    record.phone_number.match?(PHONE_REGEX)
+  end
+end
